1c6d1c961b0b76c98c2bcfed61b71dc039f55ba1
MacBook Pro <naym100m@gmail.com>
2024-05-25 20:34:27 +0600

    practice a python while loop

---
+++ Lecture5/Lecture_5.py
@@ -0,0 +1,26 @@
+""" Lecture -5 - Loops """
+
+# Loops in python explain... [while & for loops]
+
+# While loops
+
+count = 1
+
+while count <= 5:
+    # print("count")
+    count += 1
+
+    # print(count)
+
+# Print numbers from 1 to 100. <>
+i = 100
+while i >= 1:
+    # print(i)
+    i -= 1
+
+# Practice -3
+
+m = 1
+while m <= 10:
+    # print(m * 3)
+    m += 1
